a24f537746d52da7d9cba3dfb21448da5dfbc606
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-09 22:31:52 -0800

    added basic structure

--- flood-fill.cpp
+++ flood-fill.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+// #define DEBUG 1;
+
+#ifdef DEBUG
+#define IF_DEBUG(...) __VA_ARGS__
+#else
+#define IF_DEBUG(...)
+#endif
+
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color);
+
+int main(int argc, char const* argv[]) {
+  IF_DEBUG(cout << "hello world";)
   return 0;
+}
+
+vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+  vector<vector<int>> v;
+
+  return v;
 }
